f130f8d27ba3c5cd715f49fd148d38abf9bd54d8
hass <hass@85918.no-reply.drupal.org>
2009-03-04 07:17:11 +0000

    #357944: Support for try / catch

--- googleanalytics.js
+++ googleanalytics.js
@@ -17,21 +17,29 @@ Drupal.behaviors.gaTrackerAttach = function(context) {
       // Is download tracking activated and the file extension configured for download tracking?
       if (ga.trackDownload && isDownload.test(this.href)) {
         if (ga.LegacyVersion) {
+          try{
           urchinTracker(this.href.replace(isInternal, ''));
+          } catch(err) {}
         }
         else {
           // Download link clicked.
           var extension = isDownload.exec(this.href);
+          try{
           pageTracker._trackEvent("Downloads", extension[1].toUpperCase(), this.href.replace(isInternal, ''));
+          } catch(err) {}
         }
       }
       else if (isInternalSpecial.test(this.href)) {
         // Keep the internal URL for Google Analytics website overlay intact.
         if (ga.LegacyVersion) {
+          try {
           urchinTracker(this.href.replace(isInternal, ''));
+          } catch(err) {}
         }
         else {
+          try{
           pageTracker._trackPageview(this.href.replace(isInternal, ''));
+          } catch(err) {}
         }
       }
     }
@@ -39,19 +47,27 @@ Drupal.behaviors.gaTrackerAttach = function(context) {
       if (ga.trackMailto && $(this).is("a[href^=mailto:]")) {
         // Mailto link clicked.
         if (ga.LegacyVersion) {
+          try{
           urchinTracker('/mailto/' + this.href.substring(7));
+          } catch(err) {}
         }
         else {
+          try{
           pageTracker._trackEvent("Mails", "Click", this.href.substring(7));
+          } catch(err) {}
         }
       }
       else if (ga.trackOutgoing) {
         // External link clicked. Clean and track the URL.
         if (ga.LegacyVersion) {
+          try{
           urchinTracker('/outgoing/' + this.href.replace(/^(https?|ftp|news|nntp|telnet|irc|ssh|sftp|webcal):\/\//i, '').split('/').join('--'));
+          } catch(err) {}
         }
         else {
+          try{
           pageTracker._trackEvent("Outgoing links", "Click", this.href);
+          } catch(err) {}
         }
       }
     }
