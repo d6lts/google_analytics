79a03b9a18564b9d45a3a32459d72970730cb7f4
hass <hass@85918.no-reply.drupal.org>
2009-03-04 07:15:07 +0000

    #357944: Support for try / catch

--- googleanalytics.js
+++ googleanalytics.js
@@ -18,21 +18,29 @@ Drupal.behaviors.gaTrackerAttach = function(context) {
       if (ga.trackDownload && isDownload.test(this.href)) {
         // Download link clicked.
         var extension = isDownload.exec(this.href);
+        try{
         pageTracker._trackEvent("Downloads", extension[1].toUpperCase(), this.href.replace(isInternal, ''));
+        } catch(err) {}
       }
       else if (isInternalSpecial.test(this.href)) {
         // Keep the internal URL for Google Analytics website overlay intact.
+        try{
         pageTracker._trackPageview(this.href.replace(isInternal, ''));
+        } catch(err) {}
       }
     }
     else {
       if (ga.trackMailto && $(this).is("a[href^=mailto:]")) {
         // Mailto link clicked.
+        try{
         pageTracker._trackEvent("Mails", "Click", this.href.substring(7));
+        } catch(err) {}
       }
       else if (ga.trackOutgoing) {
         // External link clicked.
+        try{
         pageTracker._trackEvent("Outgoing links", "Click", this.href);
+        } catch(err) {}
       }
     }
   });
