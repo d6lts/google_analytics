4add50a9175156d8596c1ce261dce6ce5e3e60a1
hass <hass@85918.no-reply.drupal.org>
2011-02-05 19:55:47 +0000

    Remove some spaces

--- googleanalytics.js
+++ googleanalytics.js
@@ -45,7 +45,7 @@ $(document).ready(function() {
           else {
             // External link clicked.
             _gaq.push(["_trackEvent", "Outbound links", "Click", this.href]);
-  
+
             // First, delay the outbound click by a fraction of a second.
             // This delay will hardly be noticeable by the user, but it will provide the
             // browser more time load the tracking code. Without this method, it's possible
