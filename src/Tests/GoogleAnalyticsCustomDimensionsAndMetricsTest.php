729db2a5985decf9bc3810e67d9c9a7e3fc13f29
hass <hass@85918.no-reply.drupal.org>
2014-07-17 00:56:39 +0200

    Tests: Flatten the metrics and dimensions arrays

--- src/Tests/GoogleAnalyticsCustomDimensionsAndMetricsTest.php
+++ src/Tests/GoogleAnalyticsCustomDimensionsAndMetricsTest.php
@@ -46,33 +46,31 @@ class GoogleAnalyticsCustomDimensionsAndMetricsTest extends WebTestBase {
 
     // Basic test if the feature works.
     $google_analytics_custom_dimension = array(
-      'indexes' => array(
-        1 => array(
-          'index' => 1,
-          'value' => 'Bar 1',
-        ),
-        2 => array(
-          'index' => 2,
-          'value' => 'Bar 2',
-        ),
-        3 => array(
-          'index' => 3,
-          'value' => 'Bar 3',
-        ),
-        4 => array(
-          'index' => 4,
-          'value' => 'Bar 4',
-        ),
-        5 => array(
-          'index' => 5,
-          'value' => 'Bar 5',
-        ),
-      )
+      1 => array(
+        'index' => 1,
+        'value' => 'Bar 1',
+      ),
+      2 => array(
+        'index' => 2,
+        'value' => 'Bar 2',
+      ),
+      3 => array(
+        'index' => 3,
+        'value' => 'Bar 3',
+      ),
+      4 => array(
+        'index' => 4,
+        'value' => 'Bar 4',
+      ),
+      5 => array(
+        'index' => 5,
+        'value' => 'Bar 5',
+      ),
     );
     \Drupal::config('google_analytics.settings')->set('custom.dimension', $google_analytics_custom_dimension)->save();
     $this->drupalGet('');
 
-    foreach ($google_analytics_custom_dimension['indexes'] as $dimension) {
+    foreach ($google_analytics_custom_dimension as $dimension) {
       $this->assertRaw('ga("set", ' . Json::encode('dimension' . $dimension['index']) . ', ' . Json::encode($dimension['value']) . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Dimension #' . $dimension['index'] . ' is shown.');
     }
 
@@ -81,32 +79,30 @@ class GoogleAnalyticsCustomDimensionsAndMetricsTest extends WebTestBase {
     \Drupal::config('system.site')->set('slogan', $site_slogan)->save();
 
     $google_analytics_custom_dimension = array(
-      'indexes' => array(
-        1 => array(
-          'index' => 1,
-          'value' => 'Value: [site:slogan]',
-        ),
-        2 => array(
-          'index' => 2,
-          'value' => $this->randomName(16),
-        ),
-        3 => array(
-          'index' => 3,
-          'value' => '',
-        ),
-        // #2300701: Custom dimensions and custom metrics not outputed on zero value.
-        4 => array(
-          'index' => 4,
-          'value' => '0',
-        ),
-      )
+      1 => array(
+        'index' => 1,
+        'value' => 'Value: [site:slogan]',
+      ),
+      2 => array(
+        'index' => 2,
+        'value' => $this->randomName(16),
+      ),
+      3 => array(
+        'index' => 3,
+        'value' => '',
+      ),
+      // #2300701: Custom dimensions and custom metrics not outputed on zero value.
+      4 => array(
+        'index' => 4,
+        'value' => '0',
+      ),
     );
     \Drupal::config('google_analytics.settings')->set('custom.dimension', $google_analytics_custom_dimension)->save();
     $this->verbose('<pre>' . print_r($google_analytics_custom_dimension, TRUE) . '</pre>');
 
     $this->drupalGet('');
     $this->assertRaw('ga("set", ' . Json::encode('dimension1') . ', ' . Json::encode("Value: $site_slogan") . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Tokens have been replaced in dimension value.');
-    $this->assertRaw('ga("set", ' . Json::encode('dimension2') . ', ' . Json::encode($google_analytics_custom_dimension['indexes']['2']['value']) . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Random value is shown.');
+    $this->assertRaw('ga("set", ' . Json::encode('dimension2') . ', ' . Json::encode($google_analytics_custom_dimension['2']['value']) . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Random value is shown.');
     $this->assertNoRaw('ga("set", ' . Json::encode('dimension3') . ', ' . Json::encode('') . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Empty value is not shown.');
     $this->assertRaw('ga("set", ' . Json::encode('dimension4') . ', ' . Json::encode('0') . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Value 0 is shown.');
   }
@@ -117,69 +113,65 @@ class GoogleAnalyticsCustomDimensionsAndMetricsTest extends WebTestBase {
 
     // Basic test if the feature works.
     $google_analytics_custom_metric = array(
-      'indexes' => array(
-        1 => array(
-          'index' => 1,
-          'value' => '6',
-          'value_expected' => 6,
-        ),
-        2 => array(
-          'index' => 2,
-          'value' => '8000',
-          'value_expected' => 8000,
-        ),
-        3 => array(
-          'index' => 3,
-          'value' => '7.8654',
-          'value_expected' => 7.8654,
-        ),
-        4 => array(
-          'index' => 4,
-          'value' => '1123.4',
-          'value_expected' => 1123.4,
-        ),
-        5 => array(
-          'index' => 5,
-          'value' => '5,67',
-          'value_expected' => 5,
-        ),
-      )
+      1 => array(
+        'index' => 1,
+        'value' => '6',
+        'value_expected' => 6,
+      ),
+      2 => array(
+        'index' => 2,
+        'value' => '8000',
+        'value_expected' => 8000,
+      ),
+      3 => array(
+        'index' => 3,
+        'value' => '7.8654',
+        'value_expected' => 7.8654,
+      ),
+      4 => array(
+        'index' => 4,
+        'value' => '1123.4',
+        'value_expected' => 1123.4,
+      ),
+      5 => array(
+        'index' => 5,
+        'value' => '5,67',
+        'value_expected' => 5,
+      ),
     );
     \Drupal::config('google_analytics.settings')->set('custom.metric', $google_analytics_custom_metric)->save();
     $this->drupalGet('');
 
-    foreach ($google_analytics_custom_metric['indexes'] as $metric) {
+    foreach ($google_analytics_custom_metric as $metric) {
       $this->assertRaw('ga("set", ' . Json::encode('metric' . $metric['index']) . ', ' . Json::encode($metric['value_expected']) . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Metric #' . $metric['index'] . ' is shown.');
     }
 
     // Test whether tokens are replaced in custom metric values.
     $google_analytics_custom_metric = array(
-      'indexes' => array(
-        1 => array(
-          'index' => 1,
-          'value' => '[current-user:roles:count]',
-        ),
-        2 => array(
-          'index' => 2,
-          'value' => mt_rand(),
-        ),
-        3 => array(
-          'index' => 3,
-          'value' => '',
-        ),
-        // #2300701: Custom dimensions and custom metrics not outputed on zero value.
-        4 => array(
-          'index' => 4,
-          'value' => '0',
-        ),
-      )
+      1 => array(
+        'index' => 1,
+        'value' => '[current-user:roles:count]',
+      ),
+      2 => array(
+        'index' => 2,
+        'value' => mt_rand(),
+      ),
+      3 => array(
+        'index' => 3,
+        'value' => '',
+      ),
+      // #2300701: Custom dimensions and custom metrics not outputed on zero value.
+      4 => array(
+        'index' => 4,
+        'value' => '0',
+      ),
     );
     \Drupal::config('google_analytics.settings')->set('custom.metric', $google_analytics_custom_metric)->save();
     $this->verbose('<pre>' . print_r($google_analytics_custom_metric, TRUE) . '</pre>');
 
     $this->drupalGet('');
     $this->assertRaw('ga("set", ' . Json::encode('metric1') . ', ', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Tokens have been replaced in metric value.');
-    $this->assertRaw('ga("set", ' . Json::encode('metric2') . ', ' . Json::encode($google_analytics_custom_metric['indexes']['2']['value']) . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Random value is shown.');
+    $this->assertRaw('ga("set", ' . Json::encode('metric2') . ', ' . Json::encode($google_analytics_custom_metric['2']['value']) . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Random value is shown.');
     $this->assertNoRaw('ga("set", ' . Json::encode('metric3') . ', ' . Json::encode('') . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Empty value is not shown.');
     $this->assertRaw('ga("set", ' . Json::encode('metric4') . ', ' . Json::encode(0) . ');', '[testGoogleAnalyticsCustomDimensionsAndMetrics]: Value 0 is shown.');
   }
