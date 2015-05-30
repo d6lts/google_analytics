9e0097408c9064636a85ad100c66bfaeb146a4c9
hass <hass@85918.no-reply.drupal.org>
2015-05-30 13:40:29 +0200

    Issue #2355091 by hass: Change ga code style to object code style

--- google_analytics.js
+++ google_analytics.js
@@ -24,22 +24,44 @@
           // for download tracking?
           else if (drupalSettings.google_analytics.trackDownload && Drupal.google_analytics.isDownload(this.href)) {
             // Download link clicked.
-            ga("send", "event", "Downloads", Drupal.google_analytics.getDownloadExtension(this.href).toUpperCase(), {"page": Drupal.google_analytics.getPageUrl(this.href), "transport": "beacon"});
+            ga("send", {
+              "hitType": "event",
+              "eventCategory": "Downloads"
+              "eventAction": Drupal.google_analytics.getDownloadExtension(this.href).toUpperCase(),
+              "eventLabel": Drupal.google_analytics.getPageUrl(this.href),
+              "transport": "beacon"
+            });            
           }
           else if (Drupal.google_analytics.isInternalSpecial(this.href)) {
             // Keep the internal URL for Google Analytics website overlay intact.
-            ga("send", "pageview", {"page": Drupal.google_analytics.getPageUrl(this.href), "transport": "beacon"});
+            ga("send", {
+              "hitType": "pageview",
+              "page": Drupal.google_analytics.getPageUrl(this.href),
+              "transport": "beacon"
+            });
           }
         }
         else {
           if (drupalSettings.google_analytics.trackMailto && $(this).is("a[href^='mailto:'],area[href^='mailto:']")) {
             // Mailto link clicked.
-            ga("send", "event", "Mails", "Click", {"page": this.href.substring(7), "transport": "beacon"});
+            ga("send", {
+              "hitType": "event",
+              "eventCategory": "Mails"
+              "eventAction": "Click",
+              "eventLabel": this.href.substring(7),
+              "transport": "beacon"
+            });            
           }
           else if (drupalSettings.google_analytics.trackOutbound && this.href.match(/^\w+:\/\//i)) {
             if (drupalSettings.google_analytics.trackDomainMode !== 2 || (drupalSettings.google_analytics.trackDomainMode === 2 && !Drupal.google_analytics.isCrossDomain(this.hostname, drupalSettings.google_analytics.trackCrossDomains))) {
               // External link clicked / No top-level cross domain clicked.
-              ga("send", "event", "Outbound links", "Click", {"page": this.href, "transport": "beacon"});
+              ga("send", {
+                "hitType": "event",
+                "eventCategory": "Outbound links"
+                "eventAction": "Click",
+                "eventLabel": this.href,
+                "transport": "beacon"
+              });            
             }
           }
         }
@@ -49,7 +71,10 @@
     // Track hash changes as unique pageviews, if this option has been enabled.
     if (drupalSettings.google_analytics.trackUrlFragments) {
       window.onhashchange = function () {
-        ga('send', 'pageview', location.pathname + location.search + location.hash);
+        ga("send", {
+          "hitType": "pageview",
+          "page": location.pathname + location.search + location.hash,
+        });
       };
     }
 
@@ -58,7 +83,10 @@
     $(document).on("cbox_complete", function () {
       var href = $.colorbox.element().attr("href");
       if (href) {
-        ga("send", "pageview", {"page": Drupal.google_analytics.getPageUrl(href)});
+        ga("send", {
+          "hitType": "pageview",
+          "page": Drupal.google_analytics.getPageUrl(href),
+        });
       }
     });
 
